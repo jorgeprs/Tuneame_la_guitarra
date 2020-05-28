3c8249399e4f8bef37912ad8cff59f6e886d5898
Hugondon9 <62747801+Hugondon9@users.noreply.github.com>
2020-05-27 23:28:30 -0500

    Add files via upload

---
+++ recordSound.m
@@ -0,0 +1,13 @@
+function [time_vector, recordedAudio] = recordSound(Fs, duration, nBits)
+
+% Obtaining Audio
+mic = audiorecorder(Fs,nBits,1);
+disp('RECORDING');
+record(mic, duration);
+pause(duration);
+disp('RECORDING DONE');
+
+recordedAudio = getaudiodata(mic);
+% sound(recordedAudio, Fs);
+time_vector = (0:length(recordedAudio)-1)./Fs;  % Time Vector 
+end
